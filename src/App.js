8ae43ac1fcf83541b7138c4f2c1ec920e04fe88e
Nweze Daniel <flashdaniel0@gmail.com>
2022-02-11 01:55:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <h2>Breadcrumb Pagination</h2>
+      <ul className="breadcrumb">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#home">Pictures</a>
+        </li>
+        <li>
+          <a href="#home">Summer 15</a>
+        </li>
+        <li>Italy</li>
+      </ul>
+    </>
+  );
+}
